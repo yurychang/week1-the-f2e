8d6b01e5b916877833a16ae77dd23ce09dc8d8e7
Yury <yuury.chang@gmail.com>
2022-11-05 00:12:35 +0800

    add style system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'dark-bg': '#333',
+                'light-bg': '#Fafafa',
+                yellow: '#FFF1AA',
+                primary: '#F1CB04',
+                black: '#000',
+                'black-70': 'rgba(0, 0, 0, 0.7)',
+                light: '#Fafafa',
+                'light-70': 'rgba(255, 255, 255, 0.7)',
+                'primary-btn': '#F1CB04',
+                'primary-btn-hover': '#F1AF04',
+                'Secondary-btn': '#FFF1AA',
+                '$dark-card-bg': '＃424242',
+                '$dark-card-bg-hover': '#4F4F4F',
+            },
+            fontFamily: {
+                'do-hyeon': 'Do Hyeon',
+                'noto-sans': 'Noto Sans TC',
+            },
+        },
     },
     plugins: [],
 }
